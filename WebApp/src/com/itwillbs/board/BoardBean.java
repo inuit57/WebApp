76832082e289a3a2d3db69124617c75ec0c45019
narD <inuit57@naver.com>
2021-04-26 11:51:48 +0900

    게시판 관련 파일 추가

---
+++ WebApp/src/com/itwillbs/board/BoardBean.java
@@ -0,0 +1,56 @@
+package com.itwillbs.board;
+
+public class BoardBean {
+
+	int bid;  // 게시글 id
+	String uid ; //유저 이름
+	String btype ; 
+	String bsubject ; 
+	String bcontent ; 
+	int ref_id ; 
+	int comment_id ;
+	
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getBtype() {
+		return btype;
+	}
+	public void setBtype(String btype) {
+		this.btype = btype;
+	}
+	public String getBsubject() {
+		return bsubject;
+	}
+	public void setBsubject(String bsubject) {
+		this.bsubject = bsubject;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public int getRef_id() {
+		return ref_id;
+	}
+	public void setRef_id(int ref_id) {
+		this.ref_id = ref_id;
+	}
+	public int getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(int comment_id) {
+		this.comment_id = comment_id;
+	} 
+	
+}
